635c34bcf8b6b794d33f99a621357d3c7af70fe4
Anthony Liu <anthony.liu@kensho.com>
2016-11-26 14:48:05 -0500

    Add data paths to constants

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,1 +1,6 @@
+import os
+
 PROJECT_HOME = '/Users/anthonyliu/Documents/tensorfactorization'
+
+GDELT_DIR = os.path.join(PROJECT_HOME, 'data/GDELT')
+ICEWS_DIR = os.path.join(PROJECT_HOME, 'data/ICEWS')
