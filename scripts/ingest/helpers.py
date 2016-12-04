915b26d4e7fa5e982fb70a29c79a33e8c5cbfbe3
Anthony Liu <anthony.liu@kensho.com>
2016-12-04 11:28:44 -0500

    fix ingest code

--- scripts/ingest/helpers.py
+++ scripts/ingest/helpers.py
@@ -10,14 +10,27 @@ def extract_base_cameo(cameo_code):
 
 
 def parse_raw_icews(icews_datatable_fname):
-    '''Loads an icews table, converts names to shorter version, converts cameo code to base code'''
+    '''Load icews table, converts names, converts cameo code to base code (and add dummy counts)'''
     relcols = ('Event Date', 'Source Country', 'Target Country', 'CAMEO Code')
     renaming = {'Event Date': 'date', 'Source Country': 'source', 'Target Country': 'target', 'CAMEO Code': 'action'}
     icews_df = (pd.read_table(icews_datatable_fname, usecols=relcols, dtype=str, parse_dates=['Event Date'])
                 .rename(columns=renaming)
-                .assign(action=lambda df: df.action.map(extract_base_cameo)))  # Replace cameo with base code
+                .assign(action=lambda df: df.action.map(extract_base_cameo))
+                .assign(count=1))  # Replace cameo with base code
 
     return icews_df
+
+
+def parse_raw_gdelt(gdelt_datatable_fname):
+    '''Loads a gdelt table, converts names to cleaner version, converts cameo code to base code'''
+    relcols = ('Date', 'Source', 'Target', 'CAMEOCode', 'NumEvents')
+    renaming = {'Date': 'date', 'Source': 'source', 'Target': 'target', 'CAMEOCode': 'action', 
+                'NumEvents': 'count'}
+    gdelt_df = (pd.read_table(gdelt_datatable_fname, usecols=relcols, dtype=str, parse_dates=['Date'])
+                .rename(columns=renaming)
+                .assign(action=lambda df: df.action.map(extract_base_cameo)))
+
+    return gdelt_df
 
 
 def floor_year_month(dt):
@@ -30,6 +43,6 @@ def daily_data_to_monthly(df, month_count_name='count'):
     # Round data down to nearest 
     return (df
             .assign(date=lambda _df: _df.date.map(floor_year_month))  # Round dates down to month
-            .groupby(['date', 'source', 'target', 'action']).size()  # Agg by month
+            .groupby(['date', 'source', 'target', 'action'])['count'].sum()  # Agg by month
             .to_frame(month_count_name)  # Name it as 'count'
             .reset_index())
